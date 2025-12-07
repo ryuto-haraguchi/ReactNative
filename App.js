fb03cab6adaf8d07f0509709d365f1494846d6dd
Ryuto <hr48691226@gmail.com>
2025-12-07 18:10:40 +0900

    Section2-22

--- App.js
+++ App.js
@@ -1,41 +1,44 @@
-import { StyleSheet, Text, View, Button, Switch } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 import React from "react";
 
 export default function App() {
-  const [isEnabled, setIsEnabled] = React.useState(false);
-  const toggleSwitch = () => setIsEnabled((previousState) => !previousState);
-
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>
-          Open up App.js to start working on your app!
-        </Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course goal!" />
+        <Button title="Add Goal" />
       </View>
-      <Text style={styles.dummyText}>Hello, World!!!!!</Text>
-      <Button title="Tap" />
-      <Switch
-        trackColor={{ false: "#767577", true: "#81b0ff" }}
-        thumbColor="#f4f3f4"
-        ios_backgroundColor="#3e3e3e"
-        onValueChange={toggleSwitch}
-        value={isEnabled}
-      />
+      <View style={styles.goalsContainer}>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    padding: 50,
+    paddingHorizontal: 16,
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
   },
-  dummyText: {
-    margin: 16,
-    borderWidth: 3,
-    borderColor: "blue",
-    padding: 16,
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderBottomColor: "#cccccc",
+    borderBottomWidth: 1,
+    paddingBottom: 16,
+    marginBottom: 16,
+    flex: 1,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#777676ff",
+    width: "70%",
+    marginRight: 8,
+    padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
