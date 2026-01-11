dd0ea8d7714dbab17a0b8bb63cc7c65883601add
Ryuto <hr48691226@gmail.com>
2026-01-11 20:00:01 +0900

    Pressable style

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -3,10 +3,13 @@ import styles from "../styles/goalItemStyles";
 
 export default function GoalItem({ text, onDelete }) {
   return (
-    <Pressable onPress={onDelete}>
-      <View>
+    <View>
+      <Pressable
+        onPress={onDelete}
+        style={({ pressed }) => pressed && { opacity: 0.5 }}
+      >
         <Text style={styles.goalItem}>{text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
